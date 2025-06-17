cf58915605b5a1b35e7bbf34ad60dfc8d2d6f387
Thomas Csigai <thomascsigai1@gmail.com>
2025-06-17 15:29:51 +0200

    Adding a player ptr

--- SuperZefiroBros/src/scenes/DefaultScene.h
+++ SuperZefiroBros/src/scenes/DefaultScene.h
@@ -14,6 +14,7 @@ namespace ZefirApp
 		void OnLoad() override
 		{
 			APP_LOG_INFO("Scene load");
+			m_Cam.zoom = 2.0f;
 
 			AddObjectToScene(std::make_unique<ZefirApp::Background>(
 				m_EngineContext->resourceManager->GetTexture("resources\\textures\\1-1.png"))
@@ -23,7 +24,12 @@ namespace ZefirApp
 				m_EngineContext->resourceManager->GetTexture("resources\\textures\\mario.png"),
 				m_EngineContext->resourceManager->GetAnimatedTexture("resources\\anims\\mario-run.png")
 			)));
-			m_Cam.zoom = 2.0f;
+			ptr_Player = m_SceneObjects[1].get();
+
+			if (!ptr_Player)
+			{
+				APP_LOG_FATAL("Player not created.");
+			}
 		}
 
 		void OnUnload() override
@@ -33,7 +39,7 @@ namespace ZefirApp
 
 		void OnUpdate() override
 		{
-			
+			CameraUpdatePosition();
 		}
 
 		void OnSceneEvent(const SDL_Event& e)
@@ -43,5 +49,15 @@ namespace ZefirApp
 				
 			}
 		}
+
+		void CameraUpdatePosition()
+		{
+			if (ptr_Player->m_Transform2D.position.x > 0 && ptr_Player->m_Transform2D.position.x < 191)
+			{
+				m_Cam.position = m_SceneObjects[1]->m_Transform2D.position;
+			}
+		}
+
+		Zefir::GameObject* ptr_Player;
 	};
 }
